021a66e772320c3594aee648dc705f5ee9f8ec2e
Grigory Golikov <golikov@nextbi.ru>
2026-05-03 23:51:44 +0300

    Fix views track

--- core/main-service/src/main/java/ewm/event/service/EventServiceImpl.java
+++ core/main-service/src/main/java/ewm/event/service/EventServiceImpl.java
@@ -106,7 +106,7 @@ public class EventServiceImpl implements EventService {
 
         List<Event> eventList = List.of(event, event);
         try {
-            // registerHit(request);
+             registerHit(request);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         } finally {
