fc205b19ee5e244c51639343fbb5b58a95b38330
Grigory Golikov <golikov@nextbi.ru>
2026-05-04 22:06:41 +0300

    Add ddl-auto: none

--- core/main-service/src/main/resources/schema.sql
+++ core/main-service/src/main/resources/schema.sql
@@ -60,11 +60,3 @@ CREATE TABLE IF NOT EXISTS comments (
     created_on TIMESTAMP NOT NULL,
     updated_on TIMESTAMP
 );
-
-CREATE TABLE IF NOT EXISTS hits (
-    id BIGSERIAL PRIMARY KEY,
-    app VARCHAR(255) NOT NULL,
-    uri VARCHAR(255) NOT NULL,
-    ip VARCHAR(64) NOT NULL,
-    timestamp TIMESTAMP NOT NULL
-);
