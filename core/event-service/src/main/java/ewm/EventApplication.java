0d5c745826d43cf828657d17b2aeadc752039d11
Grigory Golikov <golikov@nextbi.ru>
2026-05-06 00:45:00 +0300

    Fix packaging

--- core/event-service/src/main/java/ewm/EventApplication.java
+++ core/event-service/src/main/java/ewm/EventApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"client", "ewm.user.client"})
-@ComponentScan(basePackages = {"ru.practicum", "ewm", "client"})
+@ComponentScan(basePackages = {"ru.practicum", "ewm", "client", "ewm.user"})
 public class EventApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventApplication.class, args);
